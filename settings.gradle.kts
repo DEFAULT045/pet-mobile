9ea25acf2cf02b5d7cc906a839b776912086e40f
DEFAULT045 <1687924424@QQ.com>
2026-07-31 23:13:51 +0800

    feat:增加支持

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,16 +11,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // 解决 backdrop 依赖查找失败的关键配置
     }
 }
 
-rootProject.name = "MeaPet"
+rootProject.name = "pet-mobile"
 include(":app")
